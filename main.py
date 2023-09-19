fcaea05ecffb2dbb0a682e14ca79591a2d04c718
Blue-Killer87 <104219960+Blue-Killer87@users.noreply.github.com>
2023-09-19 12:43:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,46 @@
+from kivy.base import runTouchApp
 from kivy.lang import Builder
+import kivy
 from plyer import gps
-from kivy.app import App
+from kivy.lang import Builder
 from kivy.properties import StringProperty
+from kivy.uix.widget import Widget
 from kivy.clock import mainthread
 from kivy.utils import platform
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.app import App
+import os
 
-kv = '''
-BoxLayout:
-    orientation: 'vertical'
+#os.system("py -m pip install boto3")
 
-    Label:
-        text: app.gps_location
+if __name__ == '__main__' and __package__ is None:
+    from os import sys, path
 
-    Label:
-        text: app.gps_status
+    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
-    BoxLayout:
-        size_hint_y: None
-        height: '48dp'
-        padding: '4dp'
+class WelcomeScreen(Screen):
+    pass
 
-        ToggleButton:
-            text: 'Start' if self.state == 'normal' else 'Stop'
-            on_state:
-                app.start(1000, 0) if self.state == 'down' else \
-                app.stop()
-'''
+class CreateScreen(Screen):
+    pass
+
+class ChooseScreen(Screen):
+    pass
+
+class RunScreen(Screen):
+    pass
+
+class TrackingScreen(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+
+class MyApp(App):
+    def build(self):
+        pass
 
 
 class GpsTest(App):
@@ -34,22 +49,11 @@ class GpsTest(App):
     gps_status = StringProperty('Click Start to get GPS location updates')
 
     def request_android_permissions(self):
-        """
-        Since API 23, Android requires permission to be requested at runtime.
-        This function requests permission and handles the response via a
-        callback.
 
-        The request will produce a popup if permissions have not already been
-        been granted, otherwise it will do nothing.
-        """
         from android.permissions import request_permissions, Permission
 
         def callback(permissions, results):
-            """
-            Defines the callback to be fired when runtime permission
-            has been granted or denied. This is not strictly required,
-            but added for the sake of completeness.
-            """
+
             if all([res for res in results]):
                 print("callback. All permissions granted.")
             else:
@@ -61,44 +65,6 @@ class GpsTest(App):
         # request_permissions([Permission.ACCESS_COARSE_LOCATION,
         #                      Permission.ACCESS_FINE_LOCATION])
 
-    def build(self):
-        try:
-            gps.configure(on_location=self.on_location,
-                          on_status=self.on_status)
-        except NotImplementedError:
-            import traceback
-            traceback.print_exc()
-            self.gps_status = 'GPS is not implemented for your platform'
-
-        if platform == "android":
-            print("gps.py: Android detected. Requesting permissions")
-            self.request_android_permissions()
-
-        return Builder.load_string(kv)
-
-    def start(self, minTime, minDistance):
-        gps.start(minTime, minDistance)
-
-    def stop(self):
-        gps.stop()
-
-    @mainthread
-    def on_location(self, **kwargs):
-        self.gps_location = '\n'.join([
-            '{}={}'.format(k, v) for k, v in kwargs.items()])
-
-    @mainthread
-    def on_status(self, stype, status):
-        self.gps_status = 'type={}\n{}'.format(stype, status)
-
-    def on_pause(self):
-        gps.stop()
-        return True
-
-    def on_resume(self):
-        gps.start(1000, 0)
-        pass
-
 
 if __name__ == '__main__':
-    GpsTest().run()
+    MyApp().run()
