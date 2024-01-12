e4b5340d0993ff9ffeb7085c8b6093ad0ce6edcd
BlueKiller <janvyhnanek04@seznam.cz>
2024-01-12 11:45:00 +0100

    Major overhaul.
    Major changes in overall GUI. Added confirmation screen to leaving. Preparing to add working run features. Redesigned the logo and overall color scheme. Other minor changes

--- main.py
+++ main.py
@@ -6,14 +6,23 @@ from kivy.clock import mainthread
 from kivy.utils import platform
 from plyer import gps
 from kivy.properties import StringProperty
-from kivy_garden.mapview import MapMarker, MapView
+from kivy_garden.mapview import MapMarker, MapView, MapMarkerPopup, MapLayer
 from kivy.animation import Animation
 from kivy_garden.mapview import MapSource
 from kivy.core.window import Window
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton
+from kivy.graphics.context_instructions import Translate, Scale
+from kivy.graphics import Color, Line, SmoothLine, MatrixInstruction
 
-#Window.size = (900, 800)
-#Window.minimum_width, Window.minimum_height = Window.size
-#Window.maximum_width, Window.maximum_height = (1800, 1600)
+
+if platform != "android" or platform != "ios":
+    Window.size = (900, 800)
+    Window.minimum_width, Window.minimum_height = Window.size
+
+else: 
+    pass
+
 
 class WelcomeScreen(Screen):
     pass
@@ -44,34 +53,64 @@ class Main(ScreenManager):
 
 
 class OreonApp(MDApp):
+    dialog = None
+    
+    
+
     # If platform is android, turn GPS on and set it to method on_location
     if platform == "android":
         def on_start(self):
+            pins = []
             gps.configure(on_location=self.on_location)
-            gps.start(minTime=1000, minDistance=0)
+            gps.start(minTime=2000, minDistance=0)
             print("gps.py: Android detected. Requesting permissions")
             self.request_android_permissions()
 
 
+        @mainthread
         #Main GPS method, calls itself every time the app gets new GPS telemtry
-        def on_location(self, **kwargs):
-            print(kwargs)
-            gpslan = kwargs["lat"]
-            gpslon = kwargs["lon"]
-            return gpslan, gpslon
-            
+        def on_location(self, pins, **kwargs):
+            mapview = self.root.get_screen('run').ids.mapview
+            if (kwargs['accuracy'] < 40 or kwargs['accuracy'] > 100):
+                return
+            else:
+                print(kwargs)
+                gpslat = kwargs["lat"]
+                gpslon = kwargs["lon"]
+                self.point = MapMarker(lat = gpslat, lon = gpslon)
+                pins.append(gpslat, gpslon)
+                #mapview.remove_marker(self.point)
+                mapview.add_marker(self.point)
+
+        def startRun(self, pins):
+            pass
 
         
 
+        #def center_on_gps(self, gpslan, gpslon):
+         #   print("GPS CENTER LOCATION")
+
+          #  print(f"Lan:{gpslan}, Lon:{gpslon}")
+           # mapview = self.root.get_screen('run').ids.mapview
+           # mapview.center_on(gpslan, gpslon)
+           # return gpslan, gpslon   
 
     #If platform is not Android, don't turn GPS on      
     else:
         print("Desktop version starting.")
 
-
+        def center_on_gps(self):
+            print("Not implemented in desktop version")
+            mapview = self.root.get_screen('run').ids.mapview
+            lat=50
+            lon=14
+            testpoint = MapMarker(lat=lat,lon=lon,source="data/down.png")
+            mapview.add_marker(testpoint)
+        
     def build(self):
         self.theme_cls.theme_style = "Dark" #Background
         self.theme_cls.primary_palette = "Blue" #Main color
+
 
 
     def request_android_permissions(self):
@@ -87,12 +126,34 @@ class OreonApp(MDApp):
 
         request_permissions([Permission.ACCESS_COARSE_LOCATION,
                             Permission.ACCESS_FINE_LOCATION], callback)
-        
-    def center_on_gps(self, gpslan, gpslon):
-        print("Fuck")
-        #print(f"Lan:{gpslan}, Lon:{gpslon}")
-        #mapview = self.root.get_screen('run').ids.mapview
-        #mapview.center_on(gpslan, gpslon)
     
-OreonApp().run()
+    
+    def show_alert_dialog(self):
+        if not self.dialog:
+            self.dialog = MDDialog(
+                text="Are you sure you want to stop the run? (You can't resume it later')",
+                buttons=[
+                    MDFlatButton(
+                        text="CONTINUE",
+                        theme_text_color="Custom",
+                        text_color=self.theme_cls.primary_color,
+                        on_press=self.cancelcall,
 
+                    ),
+                    MDFlatButton(
+                        text="STOP",
+                        theme_text_color="Custom",
+                        text_color=self.theme_cls.primary_color,
+                        on_press=self.discardcall,
+                    ),
+                ],
+            )
+        self.dialog.open()
+    def cancelcall(self, instance):
+        self.dialog.dismiss()
+
+    def discardcall(self, instance):
+        self.root.current = "welcome"
+        self.dialog.dismiss()
+
+OreonApp().run()
