09d9e6c3e0039f3fc940442efa06a9eeb9a1b6c9
BlueKiller <janvyhnanek04@seznam.cz>
2024-01-15 19:38:25 +0100

    Massive additions
    Added map layer feature for drawing on the map. This way you can have lines that will mark your route in later version. The code changed a lot due to this feature, adding almost 200 lines of code.

--- main.py
+++ main.py
@@ -5,17 +5,22 @@ from kivymd.app import MDApp
 from kivy.clock import mainthread
 from kivy.utils import platform
 from plyer import gps
-from kivy.properties import StringProperty
-from kivy_garden.mapview import MapMarker, MapView, MapMarkerPopup
+from kivy.properties import StringProperty, ObjectProperty
+from kivy_garden.mapview import MapMarker, MapView, MapMarkerPopup, MapLayer
 from kivy.animation import Animation
 from kivy_garden.mapview import MapSource
 from kivy.core.window import Window
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.button import MDFlatButton
-from kivy.graphics import Line
+from kivy.graphics import Line, Color, InstructionGroup
 from kivy.clock import Clock
-
-
+from kivy.graphics.context_instructions import Translate, Scale, PushMatrix, PopMatrix
+from kivy_garden.mapview.utils import clamp
+from kivy_garden.mapview.constants import \
+    (MIN_LONGITUDE, MAX_LONGITUDE, MIN_LATITUDE, MAX_LATITUDE)
+from math import radians, log, tan, cos, pi
+import random
+    
 #if platform != "android" or platform != "ios":
  #   Window.size = (900, 800)
   #  Window.minimum_width, Window.minimum_height = Window.size
@@ -49,30 +54,166 @@ class ChooseTrackScreen(Screen):
 class Main(ScreenManager):
     pass
 
+class LineDrawLayer(MapLayer):
+    def __init__(self, coordinates=[[0, 0], [0, 0]], color=[0, 0, 1, 1], **kwargs):
+        super().__init__(**kwargs)
+        self._coordinates = coordinates
+        self.color = color
+        self._line_points = None
+        self._line_points_offset = (0, 0)
+        self.zoom = 0
+        self.lon = 0
+        self.lat = 0
+        self.ms = 0
+
+    @property
+    def coordinates(self):
+        return self._coordinates
+
+    @coordinates.setter
+    def coordinates(self, coordinates):
+        self._coordinates = coordinates
+        self.invalidate_line_points()
+        self.clear_and_redraw()
+
+    @property
+    def line_points(self):
+        if self._line_points is None:
+            self.calc_line_points()
+        return self._line_points
+
+    @property
+    def line_points_offset(self):
+        if self._line_points is None:
+            self.calc_line_points()
+        return self._line_points_offset
+
+    def calc_line_points(self):
+        # Offset all points by the coordinates of the first point,
+        # to keep coordinates closer to zero.
+        # (and therefore avoid some float precision issues when drawing lines)
+        self._line_points_offset = (self.get_x(self.coordinates[0][1]),
+                                    self.get_y(self.coordinates[0][0]))
+        # Since lat is not a linear transform we must compute manually
+        self._line_points = [(self.get_x(lon) - self._line_points_offset[0],
+                              self.get_y(lat) - self._line_points_offset[1])
+                             for lat, lon in self.coordinates]
+
+    def invalidate_line_points(self):
+        self._line_points = None
+        self._line_points_offset = (0, 0)
+
+    def get_x(self, lon):
+        """Get the x position on the map using this map source's projection
+        (0, 0) is located at the top left.
+        """
+        return clamp(lon, MIN_LONGITUDE, MAX_LONGITUDE) * self.ms / 360.0
+
+    def get_y(self, lat):
+        """Get the y position on the map using this map source's projection
+        (0, 0) is located at the top left.
+        """
+        lat = radians(clamp(-lat, MIN_LATITUDE, MAX_LATITUDE))
+        return (1.0 - log(tan(lat) + 1.0 / cos(lat)) / pi) * self.ms / 2.0
+
+    # Function called when the MapView is moved
+    def reposition(self):
+        map_view = self.parent
+
+        # Must redraw when the zoom changes
+        # as the scatter transform resets for the new tiles
+        if self.zoom != map_view.zoom or \
+                   self.lon != round(map_view.lon, 7) or \
+                   self.lat != round(map_view.lat, 7):
+            map_source = map_view.map_source
+            self.ms = pow(2.0, map_view.zoom) * map_source.dp_tile_size
+            self.invalidate_line_points()
+            self.clear_and_redraw()
+
+    def clear_and_redraw(self, *args):
+        with self.canvas:
+            # Clear old line
+            self.canvas.clear()
+
+        self._draw_line()
+
+    def _draw_line(self, *args):
+        map_view = self.parent
+        self.zoom = map_view.zoom
+        self.lon = map_view.lon
+        self.lat = map_view.lat
+
+        # When zooming we must undo the current scatter transform
+        # or the animation distorts it
+        scatter = map_view._scatter
+        sx, sy, ss = scatter.x, scatter.y, scatter.scale
+
+        # Account for map source tile size and map view zoom
+        vx, vy, vs = map_view.viewport_pos[0], map_view.viewport_pos[1], map_view.scale
+
+        with self.canvas:
+
+            # Save the current coordinate space context
+            PushMatrix()
+
+            # Offset by the MapView's position in the window (always 0,0 ?)
+            Translate(*map_view.pos)
+
+            # Undo the scatter animation transform
+            Scale(1 / ss, 1 / ss, 1)
+            Translate(-sx, -sy)
+
+            # Apply the get window xy from transforms
+            Scale(vs, vs, 1)
+            Translate(-vx, -vy)
+
+            # Apply what we can factor out of the mapsource long, lat to x, y conversion
+            Translate(self.ms / 2, 0)
+
+            # Translate by the offset of the line points
+            # (this keeps the points closer to the origin)
+            Translate(*self.line_points_offset)
+
+            Color(*self.color)
+            Line(points=self.line_points, width=2)
+
+            # Retrieve the last saved coordinate space context
+            PopMatrix()
+
+
 
 class OreonApp(MDApp):
-
-
-    dialog = None
-    
     
     def build(self):
-        self.theme_cls.theme_style = "Dark" #Background
-        self.theme_cls.primary_palette = "Blue" #Main color
-        self.mapviewRun = self.root.get_screen('run').ids.mapview
+            self.theme_cls.theme_style = "Dark" #Background
+            self.theme_cls.primary_palette = "Blue" #Main color
+            self.mapviewRun = self.root.get_screen('run').ids.mapview
 
-        prague = (50.0755, 14.4378)
-        initial_position = (50.0755, 14.4378)
 
-        self.lines=[]
+            # You can import JSON data here or:
+            my_coordinates = [[51.505807, -0.128513], [51.126251, 1.327067],
+                            [50.959086, 1.827652], [48.85519, 2.35021]]
 
-        with self.mapviewRun.canvas:
-            line = Line(points=(prague[0], prague[1], initial_position[0], initial_position[1]), width=2)
-            self.lines.append(line)
+            # Red Route (multiple routes)
+            lml1 = LineDrawLayer(coordinates=my_coordinates, color=[1, 0, 0, 1])
+            self.mapviewRun.add_layer(lml1, mode="scatter")
+            
 
-        # Schedule movement updates every 5 seconds
-        Clock.schedule_interval(self.update_movement, 5)
+            # Blue route (two points)
+            my_coordinates = [[50.505807, 14.128513], [48.85519, 2.35021]]
+            lml3 = LineDrawLayer(coordinates=my_coordinates, color=[0, 0, 1, 1])
+            self.mapviewRun.add_layer(lml3, mode="scatter")
 
+        
+    # Schedule movement updates every 5 seconds
+    # Clock.schedule_interval(self.update_movement, 5)
+  
+    
+   
+
+    dialog = None
+
+    
     # If platform is android, turn GPS on and set it to method on_location
     if platform == "android":
         def on_start(self):
@@ -95,19 +236,7 @@ class OreonApp(MDApp):
                 #mapview.remove_marker(self.point)
                 self.mapviewRun.add_marker(self.point)
                 
-        
-
-        
-
-        
-
-        #def center_on_gps(self, gpslan, gpslon):
-         #   print("GPS CENTER LOCATION")
-
-          #  print(f"Lan:{gpslan}, Lon:{gpslon}")
-           # mapview = self.root.get_screen('run').ids.mapview
-           # mapview.center_on(gpslan, gpslon)
-           # return gpslan, gpslon   
+                    
 
     #If platform is not Android, don't turn GPS on      
     else:
@@ -115,26 +244,12 @@ class OreonApp(MDApp):
 
         def center_on_gps(self):
             print("Not implemented in desktop version")
-            lat=50
-            lon=14
-            testpoint = MapMarker(lat=lat,lon=lon,source="data/down.png")
-            self.mapviewRun.add_marker(testpoint)
-        def startRun(self):
-
-            with self.mapviewRun.canvas:
-               Line(points=[50, 14, 55, 18], width=2)
+ 
         
         
-
+    
     def update_movement(self, dt):
-        # New position for the updated line
-        prague = (50.0755, 14.4378)
-        new_position = (50.0765, 14.4388)
-
-        # Draw a new line connecting the previous position to the updated position
-        with self.mapviewRun.canvas:
-            line = Line(points=(prague[0], prague[1], new_position[0], new_position[1]), width=2)
-            self.lines.append(line)
+        pass
 
 
     def request_android_permissions(self):
@@ -151,6 +266,7 @@ class OreonApp(MDApp):
         request_permissions([Permission.ACCESS_COARSE_LOCATION,
                             Permission.ACCESS_FINE_LOCATION], callback)
     
+
     @mainthread
     def show_alert_dialog(self):
         if not self.dialog:
@@ -180,5 +296,17 @@ class OreonApp(MDApp):
         self.root.current = "welcome"
         self.dialog.dismiss()
 
-OreonApp().run()
 
+if __name__ == '__main__':
+    OreonApp().run()
+
+
+'''
+Otázky:
+
+Musí být sledování polohy? Je to správné?
+- Poloha skrytá, jen pro systémové využití. Trackování zobrazit až po ukončení.
+
+QR tisk na místě? Místo toho proximity checker?
+- Vytvořit předem QR kódy s číslem stanoviště. Přiřadit zahashované číslo k stanovišti na mapě. Proximity checkem zkontrolovat že tam vážně jsi.
+'''
