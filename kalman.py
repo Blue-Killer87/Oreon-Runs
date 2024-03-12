3f2240d58daacde5463714253a3ba51087d32366
BlueKiller <janvyhnanek04@seznam.cz>
2024-03-12 10:23:15 +0100

    Added script for Kalman filter
    I'm trying out to implement a simple Kalman filter for the GPS so I could get more stable routes out of it, however even the simple algorithm in this case is pretty complex and that's not even half of the problem as I have to implement it into a such large bundle of code. This will take some time to be fully working.

---
+++ kalman.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+class KalmanFilter:
+    def __init__(self, initial_state_mean, initial_state_covariance, transition_matrix, observation_matrix, observation_covariance, process_covariance):
+        self.state_mean = initial_state_mean
+        self.state_covariance = initial_state_covariance
+        self.transition_matrix = transition_matrix
+        self.observation_matrix = observation_matrix
+        self.observation_covariance = observation_covariance
+        self.process_covariance = process_covariance
+
+    def predict(self):
+        # Predict the next state
+        self.state_mean = np.dot(self.transition_matrix, self.state_mean)
+        self.state_covariance = np.dot(self.transition_matrix, np.dot(self.state_covariance, self.transition_matrix.T)) + self.process_covariance
+
+    def update(self, observation):
+        # Update the state based on observation
+        innovation = observation - np.dot(self.observation_matrix, self.state_mean)
+        innovation_covariance = np.dot(self.observation_matrix, np.dot(self.state_covariance, self.observation_matrix.T)) + self.observation_covariance
+        kalman_gain = np.dot(self.state_covariance, np.dot(self.observation_matrix.T, np.linalg.inv(innovation_covariance)))
+        self.state_mean = self.state_mean + np.dot(kalman_gain, innovation)
+        self.state_covariance = self.state_covariance - np.dot(kalman_gain, np.dot(self.observation_matrix, self.state_covariance))
+
+class KalmanSmoother:
+    def __init__(self, initial_state_mean, initial_state_covariance, transition_matrix, observation_matrix, observation_covariance, process_covariance):
+        self.filter = KalmanFilter(initial_state_mean, initial_state_covariance, transition_matrix, observation_matrix, observation_covariance, process_covariance)
+
+    def smooth(self, observations):
+        smoothed_states = []
+
+        # Run the forward pass of the Kalman filter
+        for observation in observations:
+            self.filter.predict()
+            self.filter.update(observation)
+
+        # Run the backward pass of the Kalman filter (smoother)
+        for observation in reversed(observations):
+            smoothed_states.append(self.filter.state_mean)
+            kalman_gain = np.dot(self.filter.state_covariance, np.dot(self.filter.transition_matrix.T, np.linalg.inv(self.filter.state_covariance)))
+            self.filter.state_mean = self.filter.state_mean + np.dot(kalman_gain, smoothed_states[-1] - np.dot(self.filter.transition_matrix, self.filter.state_mean))
+
+        # Reverse the list to get the correct order of smoothed states
+        return list(reversed(smoothed_states))
